463a5be1db1519e5a7a05f08791d8ca3a8ff3ae6
jay2jaypc <justin.jenkins@personetics.com>
2018-01-09 08:50:21 -0500

    package json fix2

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by gq-work on 1/9/18.
+ */
+//Install express server
+const express = require('express');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
